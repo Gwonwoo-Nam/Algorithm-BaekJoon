69a5d5adec73108921ad8867bfb39d897187c8db
Gwonwoo-Nam <ngw7617@naver.com>
2023-01-05 00:46:54 +0900

    feat(2475):검증수 해결

---
+++ prob2475/Main.java
@@ -0,0 +1,22 @@
+package prob2475;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int[] numbers = Arrays.stream(input).mapToInt(Integer::parseInt).toArray();
+
+        int sum =0;
+        for (int i=0;i<numbers.length;i++) {
+            sum += numbers[i]*numbers[i];
+        }
+        System.out.println(sum % 10);
+
+    }
+}
