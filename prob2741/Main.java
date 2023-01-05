f3054f88036161aaeb6b0017cc0d8abc9b7bbce0
Gwonwoo-Nam <ngw7617@naver.com>
2023-01-05 10:26:03 +0900

    feat(2739,2741,2753):새싹문제 3개 해결

---
+++ prob2741/Main.java
@@ -0,0 +1,17 @@
+package prob2741;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+}
