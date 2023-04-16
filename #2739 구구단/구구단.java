0cb24cec7b441c8665be9d8d70a96d2ddd53d123
Gwonwoo-Nam <112251635+Gwonwoo-Nam@users.noreply.github.com>
2023-04-16 13:10:50 +0900

    [Bronze V 2739] Title: 구구단, Time: 240 ms, Memory: 18488 KB -NKLCBHub

---
+++ #2739 구구단/구구단.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(input + " * " + i + " = " + input * i);
+        }
+
+    }
+
+}
