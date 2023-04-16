8a255dfb00cd49723fc5030b9ff7226886eeca2b
Gwonwoo-Nam <112251635+Gwonwoo-Nam@users.noreply.github.com>
2023-04-16 13:04:12 +0900

    [Silver II 1260] Title: DFS와 BFS, Time: 352 ms, Memory: 24364 KB -NKLCBHub

--- #1260 DFS와 BFS/DFS와 BFS.java
+++ #1260 DFS와 BFS/DFS와 BFS.java
@@ -1,67 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int m;
+	public static int N, M, V;
+	public static int[][] graph;
+	// public static boolean[] visit;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(stringTokenizer.nextToken());
-        m = Integer.parseInt(stringTokenizer.nextToken());
-        int v = Integer.parseInt(stringTokenizer.nextToken());
+	public static void dfs(boolean[] visit, int v) {
+		visit[v] = true;
+		System.out.print(v + " ");
+		for (int i = 1; i <= N; i++) {
+			if (!visit[i] && graph[v][i] == 1) {
+				dfs(visit, i);
+			}
+		}
 
-        boolean[][] map = new boolean[n + 1][n + 1];
-        boolean[] visited = new boolean[n + 1];
+	}
 
-        for (int i = 0; i < m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            map[a][b] = true;
-            map[b][a] = true;
-        }
+	public static void bfs(boolean[] visit) {
+		Queue<Integer> myQue = new LinkedList<>();
+		visit = new boolean[N + 1];
+		myQue.add(V);
 
-        List<Integer> dfsResult = new ArrayList<>();
-        List<Integer> bfsResult = new ArrayList<>();
-        dfs(v, visited, map, dfsResult);
-        System.out.println(dfsResult.stream().map(e -> String.valueOf(e)).reduce((a, b) -> a + " " + b).get());
-        visited = new boolean[n + 1];
-        bfs(v, visited, map, bfsResult);
-        System.out.print(bfsResult.stream().map(e -> String.valueOf(e)).reduce((a, b) -> a + " " + b).get());
-    }
+		while (!myQue.isEmpty()) {
+			int curr = myQue.poll();
 
-    public static void dfs(int v, boolean[] visited, boolean[][] map, List<Integer> dfsResult) {
-        visited[v] = true;
-        dfsResult.add(v);
-        for (int i = 1; i <= n; i++) {
-            if (map[v][i] && !visited[i])
-                dfs(i, visited, map, dfsResult);
-        }
-    }
+			if (visit[curr]) {
+				continue;
+			}
 
-    public static void bfs(int v, boolean[] visited, boolean[][] map, List<Integer> bfsResult) {
-        visited[v] = true;
-        bfsResult.add(v);
-        Queue<Integer> queue = new LinkedList();
-        queue.offer(v);
+			visit[curr] = true;
+			System.out.print(curr + " ");
 
-        while (!queue.isEmpty()) {
-            int nodeIndex = queue.poll();
-            for (int i = 1; i <= n; i++) {
-                if (map[nodeIndex][i] && !visited[i]) {
-                    queue.offer(i);
-                    visited[i] = true;
-                    bfsResult.add(i);
-                }
-            }
-        }
-    }
+			for (int i = 1; i <= N; i++) {
+				if (graph[curr][i] == 1 && !visit[i]) {
+					myQue.add(i);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // 정점의 개수
+		M = Integer.parseInt(st.nextToken()); // 간선의 개수
+		V = Integer.parseInt(st.nextToken()); // 탐색을 시작할 노드 번호
+
+		graph = new int[N + 1][N + 1];
+		boolean[] visit = new boolean[N + 1];
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int v1 = Integer.parseInt(st.nextToken());
+			int v2 = Integer.parseInt(st.nextToken());
+
+			graph[v1][v2] = 1;
+			graph[v2][v1] = 1;
+		}
+
+		dfs(visit, V);
+		System.out.println();
+		bfs(visit);
+
+	}
 }
