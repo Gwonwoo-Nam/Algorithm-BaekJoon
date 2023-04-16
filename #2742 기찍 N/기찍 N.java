884ed03fefbe13f300c373322fdaf330a37cf9a2
Gwonwoo-Nam <112251635+Gwonwoo-Nam@users.noreply.github.com>
2023-04-16 13:08:54 +0900

    [Bronze IV 2742] Title: 기찍 N, Time: 928 ms, Memory: 28636 KB -NKLCBHub

---
+++ #2742 기찍 N/기찍 N.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int j = 0;j<t;j++) {
+
+
+            System.out.println(t-j);
+        }
+
+
+
+    }
+
+}
