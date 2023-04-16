5c2365e5ec45f66409fa6d3d6b1040fdf14bd5f0
Gwonwoo-Nam <112251635+Gwonwoo-Nam@users.noreply.github.com>
2023-04-16 13:12:22 +0900

    [Bronze V 1000] Title: A+B, Time: 208 ms, Memory: 17684 KB -NKLCBHub

---
+++ #1000 A+B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        int answer = 0;
+        for (String each : input.split(" ")) {
+            answer += Integer.parseInt(each);
+        }
+        System.out.println(answer);
+    }
+}
