eca539ad3c56cd80b198fe01a7afa2194cc62721
Gwonwoo-Nam <112251635+Gwonwoo-Nam@users.noreply.github.com>
2023-07-02 14:29:36 +0900

    [Silver IV 10866] Title: 덱, Time: 72 ms, Memory: 34168 KB -NKLCBHub

---
+++ #10866 덱/덱.py
@@ -0,0 +1,42 @@
+from collections import deque
+import sys
+
+def run(deq) :
+  inputs = sys.stdin.readline().rstrip()
+  if inputs == "front" :
+    if len(deq) == 0 :
+      return -1
+    return deq[0]
+  elif inputs == "back" :
+    if len(deq) == 0 :
+      return -1
+    return deq[-1]
+  elif inputs == "size" :
+    return len(deq)
+  elif inputs == "empty" :
+    if len(deq) == 0 :
+      return 1
+    return 0
+  elif inputs == "pop_front" :
+    if len(deq) == 0 :
+      return -1
+    return deq.popleft()
+  elif inputs == "pop_back" :
+    if len(deq) == 0 :
+      return -1
+    return deq.pop()
+  elif "push_front" in inputs:
+    a = int(inputs.split()[1])
+    deq.appendleft(a)
+  else :
+    a = int(inputs.split()[1])
+    deq.append(a)
+
+
+n = int(input())
+
+deq = deque()
+for i in range(n) :
+  result = run(deq)
+  if result is not None :
+    print(result)
