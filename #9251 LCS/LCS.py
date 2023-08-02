5d78b2425d24c685db2adca1252cb6cb92c69eb2
Gwonwoo-Nam <112251635+Gwonwoo-Nam@users.noreply.github.com>
2023-08-02 20:53:36 +0900

    [Gold V 9251] Title: LCS, Time: 1004 ms, Memory: 56440 KB -NKLCBHub

---
+++ #9251 LCS/LCS.py
@@ -0,0 +1,14 @@
+a = list(input())
+b = list(input())
+
+dp = [[0 for i in range(len(b)+1)] for j in range(len(a) + 1)]
+
+ans = 0
+for i in range(1,len(a)+1) :
+  for j in range(1, len(b)+1) :
+    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+    if a[i-1] == b[j-1] :
+      dp[i][j] = dp[i-1][j-1] + 1
+    ans = max(ans, dp[i][j])
+
+print(ans)
