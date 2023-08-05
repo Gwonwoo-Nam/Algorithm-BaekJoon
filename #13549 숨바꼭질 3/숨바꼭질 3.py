87c171a43757ab359eb783e34dc37350041b5d32
Gwonwoo-Nam <112251635+Gwonwoo-Nam@users.noreply.github.com>
2023-08-05 20:25:51 +0900

    [Gold V 13549] Title: 숨바꼭질 3, Time: 308 ms, Memory: 40524 KB -NKLCBHub

--- #13549 숨바꼭질 3/숨바꼭질 3.py
+++ #13549 숨바꼭질 3/숨바꼭질 3.py
@@ -1,29 +1,25 @@
-from collections import deque
+import heapq
 
 n,k = map(int, input().split())
 
 visited = [0 for i in range(200001)]
 
-q = deque()
-q.append((n,0))
+q = []
+heapq.heappush(q, (0,n))
 visited[n] = 1
 
 while q :
-  pos, count = q.popleft()
-  tmp = pos
+  count, pos = heapq.heappop(q)
+  visited[pos] = 1
   if pos == k :
     print(count)
     break
   if pos * 2 <= 100000 and visited[pos*2] == 0:
-    visited[pos*2] = 1
-    q.append((pos*2,count))
-
-
-  if pos -1 >= 0 and visited[pos-1] == 0:
-    visited[pos-1] = 1
-    q.append((pos-1, count+1))
+    heapq.heappush(q,(count, pos*2))
   if pos + 1 <= 100000 and visited[pos+1] == 0:
-    visited[pos+1] = 1
-    q.append((pos+1, count+1))
+    heapq.heappush(q,(count+1,pos+1))
+  if pos -1 >= 0 and visited[pos-1] == 0:
+    heapq.heappush(q,(count+1,pos-1))
+
 
 
